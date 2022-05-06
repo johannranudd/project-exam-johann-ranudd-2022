f3b2a8e38b138f1e4f01cd4cc4afe1236267933c
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-05-06 19:29:22 +0200

    test image

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const url = `http://www.johannblog.one/wp-json/wp/v2/posts?_embed=true&per_page=12`;
 
+const testImg = document.querySelector('.test-img');
+
 async function getData(url) {
   const res = await fetch(url);
   const data = await res.json();
@@ -9,5 +11,6 @@ async function getData(url) {
 async function displayData() {
   const data = await getData(url);
   console.log(data);
+  testImg.src = data[0]._embedded['wp:featuredmedia'][0].source_url;
 }
 window.addEventListener('load', displayData);
