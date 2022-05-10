be390050820cc53f1c1888380ed856d699d5cedd
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-05-10 13:30:26 +0200

    styling

--- js/blogs.js
+++ js/blogs.js
@@ -65,6 +65,12 @@ async function loadeMoreFunction() {
     });
     mapData(totalData);
   } else {
+    const allPostsWarning = `<div class="all-posts-warning">All post have been leaded</div>`;
+    listOfBlogs.innerHTML += allPostsWarning;
+    setTimeout(() => {
+      const removePost = listOfBlogs.querySelector('.all-posts-warning');
+      removePost.remove();
+    }, 1000);
     return;
   }
 }
